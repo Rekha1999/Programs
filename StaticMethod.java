baf9f62c17478a845e3c460daefa35cb0bfa29fe
Rekha Anantpur <76531016+Rekha1999@users.noreply.github.com>
2023-09-26 17:37:50 +0530

    Staatic Example

---
+++ StaticMethod.java
@@ -0,0 +1,16 @@
+package com.practice.staticexample;
+
+class demo
+{
+	static void disp()
+	{
+		System.out.println("This is a disp()");
+	}
+}
+public class StaticMethod {
+
+	public static void main(String[] args) {
+		demo.disp();
+	}
+
+}
